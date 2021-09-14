1b2df044fdd7e83ad31bd77706f3aef134338353
André Pesci Cazetto <cazetto.andre@gmail.com>
2021-09-14 03:58:58 -0300

    refac

--- apps/rocketchat-desktop-frontend/src/app/App.Styled.ts
+++ apps/rocketchat-desktop-frontend/src/app/App.Styled.ts
@@ -7,34 +7,3 @@ export const Columns = styled.div`
   grid-template-columns: 100px auto;
   column-gap: 20px;
 `;
-
-export const Col1 = styled.div`
-  display: grid;
-`;
-
-export const Col2 = styled.div`
-  display: grid;
-`;
-//   grid-template-areas: "sidebar-wrapper rocketchat-wrapper";
-
-export const AppCols = styled.div`
-  grid-template-columns: 100px ;
-`;
-
-// /* fr é uma unidade fracional */
-// .grid-template-columns-2 {
-// 	grid-template-columns: 1fr 2fr;
-// }
-
-// /* minmax define um valor mínimo e máximo para a coluna */
-// .grid-template-columns-3 {
-// 	grid-template-columns: minmax(200px, 1fr) 1fr 1fr;
-// }
-
-// /* O repeat vai repetir o número de colunas */
-// .grid-template-columns-4 {
-// 	grid-template-columns: repeat(3, 1fr);
-// }
-
-// /* Auto-fit vai calcular automaticamente o total de colunas */
-// .grid-templ
--- apps/rocketchat-desktop-frontend/src/features/Webview/Webview.tsx
+++ apps/rocketchat-desktop-frontend/src/features/Webview/Webview.tsx
@@ -3,11 +3,11 @@ import * as Styled from './Webview.styled';
 
 export const Webview: FC = () => {  
   const ROCKET_CHAT_CLIENT_URL = 'https://open.rocket.chat';
-  
+  const ROCKET_CHAT_CLIENT_WEBVIEW_ID = 'rocket-chat-client-webview';
   return (
     <Styled.Wrapper>
       <Styled.Webview
-      id="rocket-chat-client-webview"
+      id={ROCKET_CHAT_CLIENT_WEBVIEW_ID}
       src={ROCKET_CHAT_CLIENT_URL}
       ></Styled.Webview>
     </Styled.Wrapper>
