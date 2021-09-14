b607ad786203384977a85487b0e9ee7dc300ff36
André Pesci Cazetto <cazetto.andre@gmail.com>
2021-09-14 04:17:29 -0300

    iframe

--- apps/rocketchat-desktop-frontend/src/app/App.Styled.ts
+++ apps/rocketchat-desktop-frontend/src/app/App.Styled.ts
@@ -4,6 +4,5 @@ export const ApplicationWrapper = styled.div``;
 
 export const Columns = styled.div`
   display: grid;
-  grid-template-columns: 100px auto;
-  column-gap: 20px;
+  grid-template-columns: 80px auto;
 `;
--- apps/rocketchat-desktop-frontend/src/app/App.tsx
+++ apps/rocketchat-desktop-frontend/src/app/App.tsx
@@ -1,13 +1,13 @@
 import * as Styled from './App.Styled';
 import { Sidebar } from '../features/Sidebar/Sidebar';
-import { Webview } from '../features/Webview/Webview';
+import { IFrame } from '../features/IFrame/IFrame';
 
 export function App() {
   return (
     <Styled.ApplicationWrapper>
       <Styled.Columns>
         <Sidebar />
-        <Webview />
+        <IFrame />
       </Styled.Columns>
     </Styled.ApplicationWrapper>
   );
