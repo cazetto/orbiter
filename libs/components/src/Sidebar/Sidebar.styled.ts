55d472eb3d4fba2f9a1b6f6a764f2dc43a809c2f
André Pesci Cazetto <cazetto.andre@gmail.com>
2021-09-20 10:09:44 -0300

    feat:(libs/components) - add sidebar component

---
+++ libs/components/src/Sidebar/Sidebar.styled.ts
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+export const SidebarWrapper = styled.div`
+  outline: 1px solid red;
+  width: 60px;
+  height: 100vh;
+`;
---
+++ libs/components/src/Sidebar/Sidebar.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import * as Styled from './Sidebar.styled';
+
+export const Sidebar: FC = () => {
+  return (
+    <Styled.SidebarWrapper>
+      Sidebar
+    </Styled.SidebarWrapper>
+  );
+};
