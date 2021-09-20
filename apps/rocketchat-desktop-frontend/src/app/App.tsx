21ba5bbf34d4618212827013233f930bbf5fac66
André Pesci Cazetto <cazetto.andre@gmail.com>
2021-09-20 17:02:30 -0300

    feat:(electron) - remove boilerplate generated with js in favor to ts

--- apps/rocketchat-desktop-frontend/src/app/App.tsx
+++ apps/rocketchat-desktop-frontend/src/app/App.tsx
@@ -4,9 +4,6 @@ import { SideBar } from '../components/SideBar/SideBar';
 import { RocketChatIframe } from '../components/RocketChatIframe/RocketChatIframe';
 
 function App() {
-  const ROCKET_CHAT_CLIENT_URL = 'https://open.rocket.chat';
-  const ROCKET_CHAT_CLIENT_IFRAME_ID = 'rocket-chat-client-iframe';
-
   return (
     <Layout>
       <SideBar>asd</SideBar>
